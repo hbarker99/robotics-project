eedda900b7cf3c4c2c6a9c4a8b961380da352706
henry <henry_barker99@hotmail.com>
2025-04-14 16:41:20 +0100

    Add simulated robot. Add initial controller attempt.

--- robotics-project/launch/launch_sim.launch.py
+++ robotics-project/launch/launch_sim.launch.py
@@ -49,7 +49,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]), 
-                    launch_arguments={'gz_args': ['-r -v4 --render-engine ogre ', world], 'on_exit_shutdown': 'true'}.items(),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items(),
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
