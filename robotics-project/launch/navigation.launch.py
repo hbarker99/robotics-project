c9fa845a9955daa93f6d2dfdfec3c2f4a5328c7c
henry <henry_barker99@hotmail.com>
2025-04-16 18:27:16 +0100

    Tidy up

--- robotics-project/launch/navigation.launch.py
+++ robotics-project/launch/navigation.launch.py
@@ -28,7 +28,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('robotics-project')
+    package_share = get_package_share_directory('robotics-project')
 
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
@@ -89,7 +89,7 @@ def generate_launch_description():
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(bringup_dir, 'config', 'nav2_params.yaml'),
+        default_value=os.path.join(package_share, 'config', 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes',
     )
 
